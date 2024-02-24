c941592858ec40230addf7709c0636e6fdf0b698
Martin Essuman <martin.essuman@gmail.com>
2024-02-24 12:23:52 +0100

    add supported macOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Network",
-    platforms: [.iOS(.v13)],
+	platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "NetworkAPI", targets: ["NetworkAPI"]),
         .library(name: "Network", targets: ["Network"]),
