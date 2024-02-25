8c46e5627c523014baa75df50d5ccd5bb45a55f2
Martin Essuman <martin.essuman@gmail.com>
2024-02-25 17:37:26 +0100

    update CI script

--- Scripts/run_unit_tests_on_ios.sh
+++ Scripts/run_unit_tests_on_ios.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
+# Scheme's name.
 SCHEME="Network-Package"
-RESULT_BUNDLE="CodeCoverage.xcresult"
-RESULT_JSON="CodeCoverage.json"
+
+# Temporary folder for storing script's generated files
+TMP_FOLDER=".scriptTemp"
+
+# File name for Xcode build results file.
+RESULT_BUNDLE="$TMP_FOLDER/CodeCoverage.xcresult"
+
+# File containing JSON with coverage data.
+RESULT_JSON="$TMP_FOLDER/CodeCoverage.json"
+
+# Minimum code coverage threshold.
 MIN_CODE_COVERAGE=99.0
 
-# RESULT_BUNDLE must always be deleted or otherwise xcodebuild will log a warning and wouldn’t create a new one.
-if [ -d $RESULT_BUNDLE ]; then
-	rm -rf $RESULT_BUNDLE
+if [ -d $TMP_FOLDER ]; then
+	echo "Removing previous $TMP_FOLDER temporary folder"
+	rm -rf $TMP_FOLDER
 fi
 
-if [ -f $RESULT_JSON ]; then
-	rm $RESULT_JSON
-fi
+# Create temp folder for temporary storage while executing the script.
+echo "Created $TMP_FOLDER folder for storing script's generated files."
+mkdir $TMP_FOLDER
 
 # The run tests section is split into build the package for testing and then running tests without building.
 # This will make easier to see which step failed.
@@ -26,13 +36,9 @@ set -o pipefail && env NSUnbufferedIO=YES xcodebuild test-without-building -sche
 
 # Fetch code coverage
 set -o pipefail && env NSUnbufferedIO=YES xcrun xccov view --report --json $RESULT_BUNDLE > $RESULT_JSON
-CODE_COVERAGE=$(cat $RESULT_JSON | jq '.targets[] | select( .name == "IndexedDataStore" and .executableLines > 0 ) | .lineCoverage')
-CODE_COVERAGE=$(echo $CODE_COVERAGE*100.0 | bc)
 
-COVERAGE_PASSES=$(echo "$CODE_COVERAGE > $MIN_CODE_COVERAGE" | bc)
-if [ $COVERAGE_PASSES -ne 1 ]; then
-	printf "\033[0;31mCode coverage %.1f%% is less than required %.1f%%\033[0m\n" $CODE_COVERAGE $MIN_CODE_COVERAGE
-	exit -1
-else
-	printf "\033[0;32mCode coverage is %.1f%%\033[0m\n" $CODE_COVERAGE
-fi
+# Check code coverage.
+./Scripts/check_code_coverage.swift $RESULT_JSON $MIN_CODE_COVERAGE
+
+echo "Removing $TMP_FOLDER temporary folder"
+rm -rf $TMP_FOLDER
